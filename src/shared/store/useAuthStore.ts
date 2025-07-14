48c11b89d18945ef44e8456ffe1ae607d42f93ec
kmj0973 <kmj0973@naver.com>
2025-07-15 01:52:44 +0900

    feat : auth 상태 관리를 위한 zustand 스토어 구현

---
+++ src/shared/store/useAuthStore.ts
@@ -0,0 +1,22 @@
+import { create } from 'zustand';
+
+type User = {
+  userId: string | null;
+  email: string | null;
+};
+
+type AuthState = {
+  accessToken: string | null;
+  user: User | null;
+  setAccessToken: (token: string) => void;
+  setUser: (user: User) => void;
+  logout: () => void;
+};
+
+export const useAuthStore = create<AuthState>((set) => ({
+  accessToken: null,
+  user: null,
+  setAccessToken: (token) => set({ accessToken: token }),
+  setUser: (user) => set({ user }),
+  logout: () => set({ accessToken: null, user: null }),
+}));
